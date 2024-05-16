0c0d22acb5fba0dca95f251c1909d7f78941e307
Tomas Lobato <Tomy@DESKTOP-226S10T>
2024-05-15 21:58:31 -0300

    first setup

---
+++ src/app/api/ask/route.ts
@@ -0,0 +1,13 @@
+import { genAI } from "@/lib/ai"
+import { NextRequest } from "next/server"
+
+export async function POST(req: NextRequest) {
+  const { prompt, prevMessages } = await req.json()
+
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" })
+
+  const result = await model.generateContent(prompt)
+  const response = result.response.text()
+
+  return new Response(response)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { genAI } from "@/lib/ai"
 import { GoogleGenerativeAI } from "@google/generative-ai"
 import { FormEvent, useState } from "react"
 
@@ -18,12 +19,10 @@ export default function Home() {
     // Add user message to messages state
     setMessages(prevMessages => [...prevMessages, { msg: prompt, from: "user" }])
 
-    const genAI = new GoogleGenerativeAI("AIzaSyBOM_iK9LuQ_DVu6vv1kxTV8AkReQ4kyGI")
     const model = genAI.getGenerativeModel({ model: "gemini-pro" })
 
     const result = await model.generateContent(prompt)
-    const response = await result.response
-    const msg = response.text()
+    const msg = result.response.text()
 
     // Add AI response to messages state
     setPrompt("")
@@ -42,8 +41,8 @@ export default function Home() {
         ))}
       </ul>
       <form action="" className="text-xl flex fixed bottom-0 p-3 border-zinc-500 border gap-1.5 bg-black" style={{ width: 672, maxWidth: "100%" }}>
-        <input type="text" className="px-3 rounded-lg w-full" placeholder="Prompt..." onChange={(ev) => setPrompt(ev.target.value)} />
-        <button onClick={(ev) => askAI(ev)} className="px-4 py-2 rounded-lg bg-zinc-900">ask</button>
+        <input type="text" className="px-3 rounded-lg w-full" placeholder="Prompt..." value={prompt} onChange={(ev) => setPrompt(ev.target.value)} />
+        <button onClick={(ev) => askAI(ev)} className="px-4 py-2 rounded-lg bg-zinc-900" disabled={prompt === ""}>ask</button>
       </form>
     </main >
   )
